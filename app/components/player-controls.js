4823b7f8d8c10e4cc3906f1e4dc5726cf5b251e9
NotGrm <notgrm@gmail.com>
2017-07-24 01:16:03 +0200

    Improve layout by adding css classes

--- app/components/player-controls.js
+++ app/components/player-controls.js
@@ -3,11 +3,19 @@ import { inject } from "@ember/service";
 import { computed, get, getProperties } from "@ember/object";
 
 export default Component.extend({
+  classNames: ['player'],
+
   hifi: inject(),
   queue: inject(),
 
+  showQueue: false,
+
   current: computed.oneWay('queue.current'),
   title: computed.alias('current.title'),
+
+  togglePause() {
+    get(this, 'hifi').togglePause();
+  },
 
   play() {
     get(this, 'hifi').togglePause();
@@ -17,8 +25,12 @@ export default Component.extend({
     get(this, 'hifi').togglePause();
   },
 
-  mute() {
+  toggleMute() {
     get(this, 'hifi').toggleMute();
+  },
+
+  toggleQueue() {
+    this.toggleProperty('showQueue');
   },
 
   skip(direction) {
