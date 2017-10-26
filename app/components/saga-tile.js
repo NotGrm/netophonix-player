992506f2c0e6d787aa162e49feb7408411dd17ad
Jeremy Bertrand <jeremy.bertrand@6cure.com>
2017-10-26 14:34:42 +0200

    Design sagas index page

--- app/components/saga-tile.js
+++ app/components/saga-tile.js
@@ -1,12 +1,23 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
-import { get } from '@ember/object';
+import { get, computed } from '@ember/object';
 
 const SagaTileComponent = Component.extend({
   tagName: 'article',
-  classNames: ['tile', 'is-child', 'notification', 'is-info'],
+  classNames: ['saga-tile'],
+  classNameBindings: ['colorClass'],
 
   router: inject(),
+
+  colorClass: computed('color', function() {
+    const color = get(this, 'color');
+
+    if(color) {
+      return `is-${color}`;
+    }
+    
+    return `is-info`
+  }),
 
   click() {
     get(this, 'router').transitionTo('sagas.show', get(this, 'saga'))
