f9a4cba3470062a96c8a30f45bc271a7729c8d95
Jeremy BERTRAND <notgrm@gmail.com>
2017-12-18 00:40:44 +0100

    Use css grid for sagas index layout

--- app/components/saga-tile.js
+++ app/components/saga-tile.js
@@ -1,23 +1,12 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
-import { get, computed } from '@ember/object';
+import { get } from '@ember/object';
 
 const SagaTileComponent = Component.extend({
   tagName: 'article',
   classNames: ['saga-tile'],
-  classNameBindings: ['colorClass'],
 
   router: inject(),
-
-  colorClass: computed('color', function() {
-    const color = get(this, 'color');
-
-    if(color) {
-      return `is-${color}`;
-    }
-    
-    return `is-info`
-  }),
 
   click() {
     get(this, 'router').transitionTo('sagas.show', get(this, 'saga'))
